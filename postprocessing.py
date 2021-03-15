38509b6bed6f63c11b14cdf39eaaf3636d47bb18
Blaine Hoak <blainehoak@gmail.com>
2021-03-15 08:41:48 -0400

    updated local copy and added files for top 50 papers

--- postprocessing.py
+++ postprocessing.py
@@ -8,6 +8,7 @@ plist = []
 for paper in papers:
     plist.append(paper)
 
+abscitepapers = {}
 # sorting by absolute citation count
 plist.sort(reverse=True, key=lambda x: x[-1][0])
 print("Absolute citation counts:")
@@ -16,9 +17,14 @@ for p in plist[:50]:
     print("Authors: ", p[3])
     print("Date: ", p[0][0])
     print("Citation Count: ", p[-1][0])
+    abscitepapers[p[-1][0]] = p[:-1]
+
+with open("abscitepapers.json", "w") as f:
+    json.dump(abscitepapers, f, sort_keys=False)
 
 print()
 print()
+relcitepapers = {}
 # sorting by relative citation count
 print("Relative citation counts:")
 plist.sort(reverse=True, key=lambda x: x[-1][1])
@@ -27,3 +33,7 @@ for p in plist[:50]:
     print("Authors: ", p[3])
     print("Date: ", p[0][0])
     print("Relative Citation Count: ", p[-1][1])
+    relcitepapers[p[-1][1]] = p[:-1]
+
+with open("relcitepapers.json", "w") as f:
+    json.dump(relcitepapers, f, sort_keys=False)
